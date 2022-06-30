2deb605605069c56465993c3627116bab2380806
pham.thanh.namb <SUN-ASTERISK\pham.thanh.namb@b123121-pc.sun-asterisk.com>
2022-06-30 11:22:23 +0700

    Add hello

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ActionController::Base
+    def hello
+        render html: "Yêu Loannnn nhiềuuuuuuuu!!! Xin lỗi Loan vì Nam đã làm Loan hiểu nhầm :((((("
+    end
+end
